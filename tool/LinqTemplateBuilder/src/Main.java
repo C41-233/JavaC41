036943c39dcb7037da3c4357ed27098486bc8fda
C41-233 <c41hahaha@163.com>
2018-01-25 23:59:02 +0800

    第三方库更新

--- tool/LinqTemplateBuilder/src/Main.java
+++ tool/LinqTemplateBuilder/src/Main.java
@@ -4,14 +4,14 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-import c41.template.parser.ITemplate;
-import c41.template.parser.TemplateParser;
+import c41.template.JustTemplate;
 
 public class Main {
 
 	public static void main(String[] args) {
 		if(args.length < 2) {
 			System.err.println("usage: [template] [config]");
+			System.exit(1);
 			return;
 		}
 		File templateFile = new File(args[0]);
@@ -19,30 +19,31 @@ public class Main {
 		
 		if(!templateFile.isFile()) {
 			System.err.println("template not exist: " + templateFile);
+			System.exit(1);
 			return;
 		}
 		if(!configFile.isFile()) {
 			System.err.println("config not exist: " + configFile);
+			System.exit(1);
 			return;
 		}
 	
 		System.out.println("generate start.");
-		TemplateParser parser = new TemplateParser();
 		try {
 			ArrayList<HashMap<String, String>> groups = GroupFileProcess.readGroups(configFile);
-			ITemplate tf = parser.parse(templateFile);
 			for (HashMap<String, String> group : groups) {
 				String filename= group.get("File");
 				System.out.println("gen "+ filename);
 				
+				String text = JustTemplate.render(templateFile, group);
 				File target = new File(templateFile.getParent(), filename);
-				String text = tf.render(group);
 				try(FileOutputStream fos = new FileOutputStream(target)){
 					fos.write(text.getBytes("utf8"));
 				}
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
+			System.exit(1);
 		}
 		
 	}
